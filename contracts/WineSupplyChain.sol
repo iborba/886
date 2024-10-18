82529e5f3d9094d85b747ca747116ecf17ecfc13
Israel Borba <israel.borba@gmail.com>
2024-10-18 18:31:15 -0300

    full workflow from producing up to delivering

--- contracts/WineSupplyChain.sol
+++ contracts/WineSupplyChain.sol
@@ -17,9 +17,9 @@ contract WineSupplyChain {
   struct Transaction {
     uint id;
     uint wineId;
-    address from;
+    address owner;
     address transporter;
-    address to;
+    address receiver;
     string status; // "pending", "completed"
   }
 
@@ -30,50 +30,89 @@ contract WineSupplyChain {
   uint public transactionCount;
 
   event WineCreated(uint256 indexed id, string name, string vineyard, string region, string grape, address owner, address producer, string status);
-  event WineSold(uint256 indexed id, address from, address to, address transporter);
+  event WineSold(uint256 indexed id, address from, address to);
   event WineShipped(uint256 indexed id, address from, address to, address transporter);
-  event WineReceived(uint256 indexed id, address from, address to, address transporter);
+  event WineDelivered(uint256 indexed id, address to);
   event WineStatusChanged(uint256 indexed id, string status);
 
   function createWine(string memory _name, string memory _vineyard, string memory _region, string memory _grape) public {
     wineCount++;
-    wines[wineCount] = Wine(wineCount, _name, _vineyard, _region, _grape, msg.sender, address(0), "available");
-    emit WineCreated(wineCount, _name, _vineyard, _region, _grape, msg.sender, address(0), "available");
+    string memory wineStatus = "available";
+    wines[wineCount] = Wine(wineCount, _name, _vineyard, _region, _grape, msg.sender, address(0), wineStatus);
+
+    emit WineCreated(wineCount, _name, _vineyard, _region, _grape, msg.sender, address(0), wineStatus);
+
+    transactionCount++;
+    transactions[transactionCount] = Transaction(transactionCount, wineCount, msg.sender, address(0), address(0), wineStatus);
   }
 
-  function sellWine(uint _id, address _to, address _transporter) public {
+  function sellWine(uint _id, address _to) public {
     Wine storage wine = wines[_id];
     require(wine.owner == msg.sender, "You are not the owner of this wine");
     require(keccak256(bytes(wines[_id].status)) == keccak256(bytes("available")), "Wine must be available for sale");
 
-    wine.owner = _to;
     wine.status = "sold";
-    transactionCount++;
-    transactions[transactionCount] = Transaction(transactionCount, _id, msg.sender, _transporter, _to, "pending");
 
-    emit WineSold(_id, msg.sender, _to, _transporter);
-    emit WineStatusChanged(_id, "sold");
+    emit WineSold(_id, msg.sender, _to);
+    emit WineStatusChanged(_id, wine.status);
+
+    transactionCount++;
+    transactions[transactionCount] = Transaction(transactionCount, _id, msg.sender, address(0), _to, wine.status);
   }
 
   function shipWine(uint _id, address _to, address _transporter) public {
     Wine storage wine = wines[_id];
     require(wine.owner == msg.sender, "You are not the owner of this wine");
+
+    wine.owner = _transporter;
     wine.status = "in transit";
-    transactionCount++;
-    transactions[transactionCount] = Transaction(transactionCount, _id, msg.sender, _transporter, _to, "pending");
+
     emit WineShipped(_id, msg.sender, _to, _transporter);
-    emit WineStatusChanged(_id, "in transit");
+    emit WineStatusChanged(_id, wine.status);
+
+    transactionCount++;
+    transactions[transactionCount] = Transaction(transactionCount, _id, _transporter, address(0), _to, wine.status);
   }
 
-  function receiveWine(uint _id, address _from, address _to, address _transporter) public {
+  function deliverWine(uint _id, address _to) public {
     Wine storage wine = wines[_id];
     require(wine.owner == msg.sender, "You are not the owner of this wine");
-    wine.status = "received";
-    emit WineReceived(_id, _from, _to, _transporter);
-    emit WineStatusChanged(_id, "received");
+
+    wine.owner = _to;
+    wine.status = "delivered";
+    emit WineDelivered(_id, _to);
+    emit WineStatusChanged(_id, wine.status);
+
+    transactionCount++;
+    transactions[transactionCount] = Transaction(transactionCount, _id, _to, msg.sender, _to, wine.status);
   }
 
   function getWine(uint _id) public view returns (Wine memory) {
     return wines[_id];
   }
+
+  function getWineTransactionHistory(uint _wineId) public view returns (Transaction[] memory) {
+      uint transactionCountForWine = 0;
+      
+      // First, count how many transactions are related to this wine
+      for (uint i = 1; i <= transactionCount; i++) {
+          if (transactions[i].wineId == _wineId) {
+              transactionCountForWine++;
+          }
+      }
+
+      // Create an array to hold the transactions
+      Transaction[] memory transactionHistory = new Transaction[](transactionCountForWine);
+      uint index = 0;
+
+      // Populate the transaction history
+      for (uint i = 1; i <= transactionCount; i++) {
+          if (transactions[i].wineId == _wineId) {
+              transactionHistory[index] = transactions[i];
+              index++;
+          }
+      }
+
+      return transactionHistory;
+    }
 }
--- test/WineSupplyChain.ts
+++ test/WineSupplyChain.ts
@@ -2,12 +2,23 @@ import hre from "hardhat";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
+interface Transaction {
+  id: number;
+  wineId: number;
+  owner: string;
+  receiver: string;
+  transporter: string;
+  status: string;
+}
+
+
 describe("WineSupplyChain", function () {
   describe("Wine Management", function () {
     let wineSupplyChain: any;
     let owner: any;
     let otherAccount: any;
     let transporterAccount: any;
+    const targetWineId = 1;
 
     before(async function () {
       const WineSupplyChain = await ethers.getContractFactory("WineSupplyChain");
@@ -17,12 +28,19 @@ describe("WineSupplyChain", function () {
       [owner, otherAccount, transporterAccount] = await hre.ethers.getSigners();
     });
 
+    // this.afterEach(async function () {
+    //   const wines = await wineSupplyChain.getWine(1);
+    //   for (let i = 0; i < wines.length; i++) {
+    //     await wineSupplyChain.connect(owner).deleteWine(1);
+    //   }
+    // });
+
     it("should create and return a new wine", async function () {
       const tx = await wineSupplyChain.connect(owner).createWine("WineName", "Vineyard", "Region", "Grape");
 
       await tx.wait();
 
-      const wine = await wineSupplyChain.getWine(1);
+      const wine = await wineSupplyChain.getWine(targetWineId);
 
       expect(wine.name).to.equal("WineName");
       expect(wine.vineyard).to.equal("Vineyard");
@@ -32,20 +50,63 @@ describe("WineSupplyChain", function () {
       expect(wine.status).to.equal("available");
     });
 
-    it.only("should sell a wine to another account", async function () {
+    it("should sell a wine to another account preserving the owner", async function () {
       const tx = await wineSupplyChain.connect(owner).createWine("WineName", "Vineyard", "Region", "Grape");
       await tx.wait();
 
-      const wine = await wineSupplyChain.getWine(1);
-
-      expect(wine.owner).to.equal(owner.address);
-      expect(wine.status).to.equal("available");
-
-      await wineSupplyChain.connect(owner).sellWine(wine.id, otherAccount.address, transporterAccount.address);
-      const soldWine = await wineSupplyChain.getWine(1);
+      await wineSupplyChain.connect(owner).sellWine(targetWineId, otherAccount.address);
+      const soldWine = await wineSupplyChain.getWine(targetWineId);
       
-      expect(soldWine.owner).to.equal(otherAccount.address);
+      expect(soldWine.owner).to.equal(owner.address);
       expect(soldWine.status).to.equal("sold");
+    });
+
+    it("should transport a wine to the consumer", async function () {
+      const tx = await wineSupplyChain.connect(owner).createWine("WineName", "Vineyard", "Region", "Grape");
+      await tx.wait();
+
+      await wineSupplyChain.connect(owner).sellWine(targetWineId, otherAccount.address);
+      await wineSupplyChain.connect(owner).shipWine(targetWineId, otherAccount.address, transporterAccount.address);
+      const transportedWine = await wineSupplyChain.getWine(targetWineId);
+
+      expect(transportedWine.owner).to.equal(transporterAccount.address);
+      expect(transportedWine.status).to.equal("in transit");
+    });
+
+    it("should deliver a wine to the consumer", async function () {
+      const tx = await wineSupplyChain.connect(owner).createWine("WineName", "Vineyard", "Region", "Grape");
+      await tx.wait();
+
+      await wineSupplyChain.connect(owner).sellWine(targetWineId, otherAccount.address);
+      await wineSupplyChain.connect(owner).shipWine(targetWineId, otherAccount.address, transporterAccount.address);
+      await wineSupplyChain.connect(transporterAccount).deliverWine(targetWineId, otherAccount.address);
+      const deliveredWine = await wineSupplyChain.getWine(targetWineId);
+
+      expect(deliveredWine.owner).to.equal(otherAccount.address);
+      expect(deliveredWine.status).to.equal("delivered");
+    });
+
+    it.only('should trace wine history', async function () {
+      const tx = await wineSupplyChain.connect(owner).createWine("WineName", "Vineyard", "Region", "Grape");
+      await tx.wait();
+
+      await wineSupplyChain.connect(owner).sellWine(targetWineId, otherAccount.address);
+      await wineSupplyChain.connect(owner).shipWine(targetWineId, otherAccount.address, transporterAccount.address);
+      await wineSupplyChain.connect(transporterAccount).deliverWine(targetWineId, otherAccount.address);
+
+      const wineHistory: Transaction[] = await wineSupplyChain.getWineTransactionHistory(targetWineId);
+
+      expect(wineHistory[0].owner).to.equal(owner.address);
+      expect(wineHistory[0].status).to.equal("available");
+
+      expect(wineHistory[1].owner).to.equal(owner.address);
+      expect(wineHistory[1].status).to.equal("sold");
+
+      expect(wineHistory[2].owner).to.equal(transporterAccount.address);
+      expect(wineHistory[2].status).to.equal("in transit");
+
+      expect(wineHistory[3].owner).to.equal(otherAccount.address);
+      expect(wineHistory[3].status).to.equal("delivered");
     });
   });
 });
