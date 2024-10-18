41228d7c4562d5c39f8b36b3f0146ea413dd6b50
Israel Borba <israel.borba@gmail.com>
2024-10-18 17:29:29 -0300

    test selling a wine

--- contracts/WineSupplyChain.sol
+++ contracts/WineSupplyChain.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "hardhat/console.sol";
 
 contract WineSupplyChain {
   struct Wine {
@@ -10,18 +11,66 @@ contract WineSupplyChain {
     string grape;
     address owner;
     address producer;
-    string status; // "created", "produced", "bottled", "sold", "shipped", "received"
+    string status; // "created", "produced", "bottled", "sold", "shipped", "in transit",, "received"
+  }
+
+  struct Transaction {
+    uint id;
+    uint wineId;
+    address from;
+    address transporter;
+    address to;
+    string status; // "pending", "completed"
   }
 
   mapping(uint => Wine) public wines;
+  mapping(uint => Transaction) public transactions;
+  
   uint public wineCount;
+  uint public transactionCount;
 
-  event WineCreated(uint id, string name, string vineyard, string region, string grape, address owner, address producer, string status);
+  event WineCreated(uint256 indexed id, string name, string vineyard, string region, string grape, address owner, address producer, string status);
+  event WineSold(uint256 indexed id, address from, address to, address transporter);
+  event WineShipped(uint256 indexed id, address from, address to, address transporter);
+  event WineReceived(uint256 indexed id, address from, address to, address transporter);
+  event WineStatusChanged(uint256 indexed id, string status);
 
   function createWine(string memory _name, string memory _vineyard, string memory _region, string memory _grape) public {
     wineCount++;
-    wines[wineCount] = Wine(wineCount, _name, _vineyard, _region, _grape, msg.sender, address(0), "created");
-    emit WineCreated(wineCount, _name, _vineyard, _region, _grape, msg.sender, address(0), "created");
+    wines[wineCount] = Wine(wineCount, _name, _vineyard, _region, _grape, msg.sender, address(0), "available");
+    emit WineCreated(wineCount, _name, _vineyard, _region, _grape, msg.sender, address(0), "available");
+  }
+
+  function sellWine(uint _id, address _to, address _transporter) public {
+    Wine storage wine = wines[_id];
+    require(wine.owner == msg.sender, "You are not the owner of this wine");
+    require(keccak256(bytes(wines[_id].status)) == keccak256(bytes("available")), "Wine must be available for sale");
+
+    wine.owner = _to;
+    wine.status = "sold";
+    transactionCount++;
+    transactions[transactionCount] = Transaction(transactionCount, _id, msg.sender, _transporter, _to, "pending");
+
+    emit WineSold(_id, msg.sender, _to, _transporter);
+    emit WineStatusChanged(_id, "sold");
+  }
+
+  function shipWine(uint _id, address _to, address _transporter) public {
+    Wine storage wine = wines[_id];
+    require(wine.owner == msg.sender, "You are not the owner of this wine");
+    wine.status = "in transit";
+    transactionCount++;
+    transactions[transactionCount] = Transaction(transactionCount, _id, msg.sender, _transporter, _to, "pending");
+    emit WineShipped(_id, msg.sender, _to, _transporter);
+    emit WineStatusChanged(_id, "in transit");
+  }
+
+  function receiveWine(uint _id, address _from, address _to, address _transporter) public {
+    Wine storage wine = wines[_id];
+    require(wine.owner == msg.sender, "You are not the owner of this wine");
+    wine.status = "received";
+    emit WineReceived(_id, _from, _to, _transporter);
+    emit WineStatusChanged(_id, "received");
   }
 
   function getWine(uint _id) public view returns (Wine memory) {
--- test/WineSupplyChain.ts
+++ test/WineSupplyChain.ts
@@ -1,29 +1,24 @@
-// import {
-//   time,
-//   loadFixture,
-// } from "@nomicfoundation/hardhat-toolbox/network-helpers";
-// import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import hre from "hardhat";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { Contract } from "ethers";
 
 describe("WineSupplyChain", function () {
   describe("Wine Management", function () {
     let wineSupplyChain: any;
     let owner: any;
     let otherAccount: any;
+    let transporterAccount: any;
 
     before(async function () {
       const WineSupplyChain = await ethers.getContractFactory("WineSupplyChain");
       wineSupplyChain = await WineSupplyChain.deploy();
 
       await wineSupplyChain.waitForDeployment(); 
-      [owner, otherAccount] = await hre.ethers.getSigners();
+      [owner, otherAccount, transporterAccount] = await hre.ethers.getSigners();
     });
 
     it("should create and return a new wine", async function () {
-      const tx = await wineSupplyChain.connect(otherAccount).createWine("WineName", "Vineyard", "Region", "Grape");
+      const tx = await wineSupplyChain.connect(owner).createWine("WineName", "Vineyard", "Region", "Grape");
 
       await tx.wait();
 
@@ -33,8 +28,24 @@ describe("WineSupplyChain", function () {
       expect(wine.vineyard).to.equal("Vineyard");
       expect(wine.region).to.equal("Region");
       expect(wine.grape).to.equal("Grape");
-      expect(wine.owner).to.equal(otherAccount.address);
-      expect(wine.status).to.equal("created");
+      expect(wine.owner).to.equal(owner.address);
+      expect(wine.status).to.equal("available");
+    });
+
+    it.only("should sell a wine to another account", async function () {
+      const tx = await wineSupplyChain.connect(owner).createWine("WineName", "Vineyard", "Region", "Grape");
+      await tx.wait();
+
+      const wine = await wineSupplyChain.getWine(1);
+
+      expect(wine.owner).to.equal(owner.address);
+      expect(wine.status).to.equal("available");
+
+      await wineSupplyChain.connect(owner).sellWine(wine.id, otherAccount.address, transporterAccount.address);
+      const soldWine = await wineSupplyChain.getWine(1);
+      
+      expect(soldWine.owner).to.equal(otherAccount.address);
+      expect(soldWine.status).to.equal("sold");
     });
   });
 });
